fb1ecd426db795e86776bfe94d353be81b4accd9
Subx1s0o <nazarsabardin@gmail.com>
2024-08-23 18:20:21 +0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import "./css/App.css";
-import ContactForm from "./Components/ContactForm/ContactForm";
-import ContactList from "./Components/ContactList/ContactList";
-import SearchBox from "./Components/SearchBox/SearchBox";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
+import ContactForm from "./cc/ContactForm/ContactForm";
+import ContactList from "./cc/ContactList/ContactList";
+import SearchBox from "./cc/SearchBox/SearchBox";
+import "./css/App.css";
 import { fetchContacts } from "./redux/contactsOps";
 
 function App() {
